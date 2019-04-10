5a0ca06848553308a2e9e00a0cbc1735bba37c07
Abhimanyu Kapur <abhi.kapur09@gmail.com>
2019-04-10 17:37:58 -0400

    Finished up with setup

--- src/index.js
+++ src/index.js
@@ -1,1 +1,8 @@
-console.log('starting up!');
+import $ from 'jquery';
+import './style.scss';
+
+let num = 0;
+setInterval(() => {
+  num += 1;
+  $('#main').html(`You've been on this page for ${num} seconds.`);
+}, 1000);
