4d3b9cc7d229e3e9c7a9860ceea1f8833beeb39d
Abhimanyu Kapur <abhi.kapur09@gmail.com>
2019-04-13 14:59:04 -0400

    Finished setup, ready to push to starterpack

--- src/index.js
+++ src/index.js
@@ -1,8 +1,7 @@
-import $ from 'jquery';
+import React from 'react';
+import ReactDOM from 'react-dom';
 import './style.scss';
 
-let num = 0;
-setInterval(() => {
-  num += 1;
-  $('#main').html(`You've been on this page for ${num} seconds.`);
-}, 1000);
+const App = () => <div className="test">All the REACT are belong to us!</div>;
+
+ReactDOM.render(<App />, document.getElementById('main'));
